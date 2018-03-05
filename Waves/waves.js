73b3015b0328b881256da94d1cc18fe4f6ebf367
Yash Tripathi <tripathiyash97@gmail.com>
2018-03-05 12:54:12 +0530

    Better...

--- Waves/waves.js
+++ Waves/waves.js
@@ -1,27 +1,23 @@
-function sig( angle){
-  return (1/(1+exp(angle*-1)));
-}
-
 function setup() {
-  createCanvas( PI*200, PI*200);
+  createCanvas( 800, 600);
   background(0);
   noFill();
   strokeWeight(1);
   stroke(255, 80, 0);
   line( width/2, 0, width/2, height);
   line( 0, height/2, width, height/2);
-  var baseValue = -TWO_PI;
-  var inr = TWO_PI/50;
+  line( 1, 0, 1, height);
   stroke(80, 255, 0);
   strokeWeight(3);
+  translate( width/2, height/2);
   beginShape();
-  for (var x = 0; x <= width; x += PI*2) {
-    var y = map(sig(baseValue),1,-1,0,height);
-    vertex(x,y);
-    baseValue += inr;
-  } 
-  endShape(); 
+  for( var x = -width/2; x <= width/2; x += width/128){
+    var y = map( exp(x*PI/360), 10, -10, -height/2, height/2);
+    vertex( x, y);
+  }
+  endShape();
 }
 
 function draw() {
+  
 }
